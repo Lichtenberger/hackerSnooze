0ab9ac5efcad4ebe8fb5914f61566e0116a9efbd
Mike <124381569+Lichtenberger@users.noreply.github.com>
2023-10-26 09:44:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// So we don't have to keep re-finding things on page, find DOM elements once:
 
 const $body = $("body");
 
@@ -11,6 +10,7 @@ const $ownStories = $("#my-stories");
 const $storiesContainer = $("#stories-container");
 
 const $storiesLists = $(".stories-list");
+
 const $loginForm = $("#login-form");
 const $signupForm = $("#signup-form");
 const $submitForm = $("#submit-form");
@@ -22,10 +22,6 @@ const $navSubmitStory = $("#nav-submit-story");
 
 const $userProfile = $("#user-profile");
 
-/** To make it easier for individual components to show just themselves, this
- * is a useful function that hides pretty much everything on the page. After
- * calling this, individual components can re-show just what they want.
- */
 
 function hidePageComponents() {
   const components = [
@@ -38,20 +34,15 @@ function hidePageComponents() {
   components.forEach(c => c.hide());
 }
 
-/** Overall function to kick off the app. */
-
 async function start() {
   console.debug("start");
 
-  // "Remember logged-in user" and log in, if credentials in localStorage
   await checkForRememberedUser();
   await getAndShowStoriesOnStart();
 
-  // if we got a logged-in user
   if (currentUser) updateUIOnUserLogin();
 }
 
-// Once the DOM is entirely loaded, begin the app
 
 console.warn("HEY STUDENT: This program sends many debug messages to" +
   " the console. If you don't see the message 'start' below this, you're not" +
